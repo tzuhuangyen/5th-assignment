252fd93b8d0bacb96c064faac6ef098d14624d50
TzuHuang Yen <yan0912@hotmail.com>
2022-08-18 21:16:54 +0200

    Update 2022-08-18T19:16:45.479Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,13 +1,15 @@
-$(document).ready(() => {
+"use strict";
+
+$(document).ready(function () {
   $(".reply-btn").click(function () {
     $(".replied").addClass("d-none");
     $(".footer-btn").addClass("d-none");
     $(".reply-form").show();
   });
-
   $(".reply-form-btn-cancel").click(function () {
     $(".replied").removeClass("d-none");
     $(".footer-btn").removeClass("d-none");
     $(".reply-form").hide();
   });
 });
+//# sourceMappingURL=all.js.map
